611412bfaaeb30d072aa0e56de6a835a35992bf9
Fabio Rauh <fabio.rauh@study.thws.de>
2025-08-24 18:40:57 +0200

    Delete TheterveilParts directory

--- TheterveilParts/game.js
+++
@@ -1,6 +0,0 @@
-import { SceneManager } from './scripts/sceneManager.js';
-
-
-const gameContainer = document.getElementById('game');
-const sceneManager = new SceneManager(gameContainer);
-sceneManager.loadScene('menu');
