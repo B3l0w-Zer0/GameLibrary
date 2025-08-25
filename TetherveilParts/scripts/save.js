e10114155ca92a7842fc11d40e9dee59fa1a4ac4
Fabio Rauh <fabio.rauh@study.thws.de>
2025-08-25 17:24:10 +0200

    Delete TetherveilParts directory

--- TetherveilParts/scripts/save.js
+++
@@ -1,8 +0,0 @@
-export function saveGame(playerData) {
-  localStorage.setItem('theterveilSave', JSON.stringify(playerData));
-}
-
-export function loadGame() {
-  const data = localStorage.getItem('theterveilSave');
-  return data ? JSON.parse(data) : null;
-}
--- TetherveilParts/scripts/vessel.js
+++
@@ -1,7 +0,0 @@
-export class Vessel {
-  constructor(data) {
-    this.name = data.name;
-    this.power = data.power;
-    this.description = data.description;
-  }
-}
