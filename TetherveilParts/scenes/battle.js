9ef56dec5377531ad9a9800fc93620380b2b2146
Jan Hannig <janhannig210705@gmail.com>
2025-08-26 21:23:30 +0200

    Update battle.js

--- TetherveilParts/scenes/battle.js
+++ TetherveilParts/scenes/battle.js
@@ -1,3 +1,11 @@
-export function render(container, sceneManger){
+import { createBag } from '../battle/bag.js';
+
+export async function render(container, sceneManager) {
+    const wrapper = createBag();
+
+    const canvas = document.getElementById("gameCanvas");
+    if (canvas) canvas.style.display = "none";
+    container.appendChild(wrapper);
 
 }
+
